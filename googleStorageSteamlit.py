ac8cb196970cffffe0cbfa43b163b5f99a71b011
retipek <74456434+retipek@users.noreply.github.com>
2022-06-09 01:30:15 +0200

    Update googleStorageSteamlit.py

--- googleStorageSteamlit.py
+++ googleStorageSteamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from google.oauth2 import service_account
+#from google.oauth2 import service_account
 from google.cloud import storage
 
 st.title('Test wizualizacji na wbe serwerze')
