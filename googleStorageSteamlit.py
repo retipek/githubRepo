5af3cad8003174291a1c0ddd65700084e5896877
retipek <74456434+retipek@users.noreply.github.com>
2022-06-09 01:28:34 +0200

    Update googleStorageSteamlit.py

--- googleStorageSteamlit.py
+++ googleStorageSteamlit.py
@@ -2,6 +2,7 @@ import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import storage
 
+st.title('Test wizualizacji na wbe serwerze')
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gpc_service_account"]
